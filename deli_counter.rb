dd32f4e263a2b4d1567db70805a6334cdb2db8b5
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-13 15:25:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.empty? 
+    puts "The line is currently empty."
+  else 
+    string = "The line is currently:"
+    katz_deli.each_with_index do |name, spot|
+      string += " #{spot + 1}. #{name}"
+  end
+  puts string
+end
+
+def take_a_number[katz_deli, string]
+
+end
