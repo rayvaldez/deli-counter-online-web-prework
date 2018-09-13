2cbbc7263adfc0c711e758ae8b31accc2f2abe37
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-13 15:25:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.empty? 
+  if katz_deli.empty?
     puts "The line is currently empty."
-  else 
+  else
     string = "The line is currently:"
     katz_deli.each_with_index do |name, spot|
       string += " #{spot + 1}. #{name}"
