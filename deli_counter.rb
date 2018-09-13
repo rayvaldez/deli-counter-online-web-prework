21fe574c5cf088aff64a12609979945420bdf20c
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-13 15:39:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,11 @@ def take_a_number(katz_deli, string)
   number_in_line = katz_deli.index(string)
   puts "Welcome, #{string}. You are number #{number_in_line + 1} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+end
